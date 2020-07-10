d45644bf5e1867eb4d021aa2d5d33d7d610ba033
abdullah <45887687+heartwhite@users.noreply.github.com>
2020-07-10 14:53:39 +0200

    Add files via upload
    first commit

---
+++ build/precache-manifest.b2688a7ffd44d4ee8e7c5c53df225248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2c2ca87c2419b520c2d2e9be4d13c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8147b9ffdecb4aeea5a9",
+    "url": "/static/css/main.163f4f25.chunk.css"
+  },
+  {
+    "revision": "39cfec79aa11a2da63b4",
+    "url": "/static/js/2.c43803e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c43803e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8147b9ffdecb4aeea5a9",
+    "url": "/static/js/main.73c0fddb.chunk.js"
+  },
+  {
+    "revision": "a3d2bf21716328fc74a3",
+    "url": "/static/js/runtime-main.d8499093.js"
+  }
+]);
